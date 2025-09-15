8e7ee2a5159bc757ae12e4a93063aa9cb1bfab18
Michael Lively <milively@microsoft.com>
2025-09-15 15:12:52 -0700

    wip

--- src/vs/workbench/contrib/issue/browser/issue.contribution.ts
+++ src/vs/workbench/contrib/issue/browser/issue.contribution.ts
@@ -4,63 +4,38 @@
  *--------------------------------------------------------------------------------------------*/
 
 import * as nls from '../../../../nls.js';
+import { localize } from '../../../../nls.js';
 import { CommandsRegistry } from '../../../../platform/commands/common/commands.js';
 import { IConfigurationService } from '../../../../platform/configuration/common/configuration.js';
 import { Extensions as ConfigurationExtensions, IConfigurationRegistry } from '../../../../platform/configuration/common/configurationRegistry.js';
+import { SyncDescriptor } from '../../../../platform/instantiation/common/descriptors.js';
 import { InstantiationType, registerSingleton } from '../../../../platform/instantiation/common/extensions.js';
 import { IInstantiationService } from '../../../../platform/instantiation/common/instantiation.js';
 import { IProductService } from '../../../../platform/product/common/productService.js';
 import { Registry } from '../../../../platform/registry/common/platform.js';
-import { Extensions, IWorkbenchContributionsRegistry, IWorkbenchContribution, registerWorkbenchContribution2, WorkbenchPhase } from '../../../common/contributions.js';
-import { IEditorSerializer, EditorExtensions, IEditorFactoryRegistry } from '../../../common/editor.js';
+import { EditorPaneDescriptor, IEditorPaneRegistry } from '../../../browser/editor.js';
+import { Extensions, IWorkbenchContributionsRegistry } from '../../../common/contributions.js';
+import { EditorExtensions, IEditorFactoryRegistry, IEditorSerializer } from '../../../common/editor.js';
 import { EditorInput } from '../../../common/editor/editorInput.js';
 import { LifecyclePhase } from '../../../services/lifecycle/common/lifecycle.js';
-import { IEditorResolverService, RegisteredEditorPriority } from '../../../services/editor/common/editorResolverService.js';
+import { BaseIssueContribution } from '../common/issue.contribution.js';
+import { IIssueFormService, IWorkbenchIssueService } from '../common/issue.js';
 import { IssueFormService } from './issueFormService.js';
+import { IssueReporterEditor } from './issueReporterEditor.js';
+import { IssueReporterEditorInput } from './issueReporterEditorInput.js';
 import { BrowserIssueService } from './issueService.js';
 import './issueTroubleshoot.js';
-import { IIssueFormService, IWorkbenchIssueService } from '../common/issue.js';
-import { BaseIssueContribution } from '../common/issue.contribution.js';
-import { IssueReporterEditorInput } from './issueReporterEditorInput.js';
-import { localize } from '../../../../nls.js';
+
+//#region --- register issue reporter editor pane
+
+Registry.as<IEditorPaneRegistry>(EditorExtensions.EditorPane).registerEditorPane(
+	EditorPaneDescriptor.create(IssueReporterEditor, IssueReporterEditor.ID, localize('issueReporter', "Issue Reporter")),
+	[new SyncDescriptor(IssueReporterEditorInput)]
+);
+
+//#endregion
 
 //#region --- issue reporter editor
-
-class IssueReporterEditorContribution implements IWorkbenchContribution {
-
-	static readonly ID = 'workbench.contrib.issueReporterEditor';
-
-	constructor(
-		@IEditorResolverService editorResolverService: IEditorResolverService,
-		@IInstantiationService instantiationService: IInstantiationService
-	) {
-		editorResolverService.registerEditor(
-			`${IssueReporterEditorInput.RESOURCE.scheme}:**/**`,
-			{
-				id: IssueReporterEditorInput.ID,
-				label: localize('promptOpenWith.issueReporter.displayName', "Issue Reporter"),
-				priority: RegisteredEditorPriority.exclusive
-			},
-			{
-				singlePerResource: false,
-				canSupportResource: resource => resource.scheme === IssueReporterEditorInput.RESOURCE.scheme
-			},
-			{
-				createEditorInput: ({ resource, options }) => {
-					return {
-						editor: IssueReporterEditorInput.getInstance(),
-						options: {
-							pinned: true,
-							...options
-						}
-					};
-				}
-			}
-		);
-	}
-}
-
-registerWorkbenchContribution2(IssueReporterEditorContribution.ID, IssueReporterEditorContribution, WorkbenchPhase.BlockStartup);
 
 class IssueReporterEditorInputSerializer implements IEditorSerializer {
 
